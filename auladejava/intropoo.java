0a3944e8693238b4fe8d9fb2afeea19ea0686036
RodrigoBerino <r9.berino@gmail.com>
2021-06-28 22:18:25 -0300

    intro

---
+++ auladejava/intropoo.java
@@ -0,0 +1,16 @@
+class intropoo {
+    public static void main (String [] args){
+        
+    }
+}
+
+/* Vantagens de oo
+- reuso de cod
+- reflete o mundo real
+- facilita a manutenção*/
+
+/*conceitos
+-classes : descrição de um grupo de objetos - nome da classe, conj de atributos e conj de metodos = comportamento/ações
+-objetos : 
+-herança
+-poliformismo*/
